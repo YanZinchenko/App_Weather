584eeebb098c7b190327022d47673a18bafab160
Y@n <Zinchenko.yan@gmail.com>
2022-12-20 20:25:25 +0200

    fetch with useEffect

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import './App.css';
 
 import { Input } from './Input';
-import { Card } from './Card';
+import { CardList } from './CardList';
 
 function App() {
 	const [citiesList, setCitiesList] = useState([]);
@@ -10,11 +10,7 @@ function App() {
 	return (
 		<div className='Main'>
 			<Input setCitiesList={setCitiesList} />
-			<div className='CardList'>
-				{citiesList.map(city => (
-					<Card key={city} city={city} />
-				))}
-			</div>
+			<CardList citiesList={citiesList} />
 		</div>
 	);
 }
---
+++ src/settings.js
@@ -0,0 +1,1 @@
+export const API_KEY = 'e5288d526fde609640a8ab22c9f7358c';
